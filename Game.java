4fa8e4447a12cf5a7b84df25bb21016ed6d46490
unknown <iro85x@gmail.com>
2019-06-09 17:53:30 +0200

    commit r01

--- Game.java
+++ Game.java
@@ -19,8 +19,16 @@ import java.util.*;
 public class Game 
 {
     private Parser parser;
+    // Habitacion actual
     private Room currentRoom;
+    // Pila donde guardamos las habitaciones por las que hemos ido pasando
     private Stack<Room> previousRoom;
+    // ArryList donde guardamos los objetos que transportamos
+    private ArrayList<Item> itemBackpack;
+    // Peso maximo que podemos transportar
+    private static final int MAX_WEIGHT = 30;
+    //distancia entre el nombre de item y su peso
+    private static final int DISTANCE = 12;
 
     /**
      * Create the game and initialise its internal map.
@@ -30,6 +38,7 @@ public class Game
         createRooms();
         parser = new Parser();
         previousRoom = new Stack<Room>();
+        itemBackpack = new ArrayList<Item>();
     }
 
     /**
@@ -50,16 +59,23 @@ public class Game
         pozo = new Room("el pozo");
         herreria = new Room("la herreria");
         puertaDeEntrada = new Room("la puerta de entrada");
-        
+
         // inicializamos los objetos de las salas
         calabozo.addItem(new Item ("cadenas", 10));
-        patio.addItem(new Item ("banco de madera", 20));
+        calabozo.addItem(new Item ("grilletes", 5));
+        patio.addItem(new Item ("banco", 30));
         establos.addItem(new Item ("montura", 5));
         granSalonReal.addItem(new Item ("trono", 50));
         capilla.addItem(new Item ("cruz", 25));
+        capilla.addItem(new Item ("caliz", 5));
+        capilla.addItem(new Item ("bancos", 60));
         cocina.addItem(new Item ("cazuela", 10));
+        cocina.addItem(new Item ("mesa", 20));
+        cocina.addItem(new Item ("sarten", 5));
         herreria.addItem(new Item ("maza", 15));
-        
+        herreria.addItem(new Item ("espada", 10));
+        herreria.addItem(new Item ("escudo", 10));
+
         // initialise room exits
         calabozo.setExit("north", torreon);
         torreon.setExit("north", pozo);
@@ -99,7 +115,7 @@ public class Game
             Command command = parser.getCommand();
             finished = processCommand(command);
         }
-        System.out.println("Thank you for playing.  Good bye.");
+        System.out.println("Gracias por jugar. Hasta luego.");
     }
 
     /**
@@ -108,9 +124,9 @@ public class Game
     private void printWelcome()
     {
         System.out.println();
-        System.out.println("Welcome to the World of Zuul!");
-        System.out.println("World of Zuul is a new, incredibly boring adventure game.");
-        System.out.println("Type 'help' if you need help.");
+        System.out.println("¡¡¡Bienvenido al mundo de Zuul!!!");
+        System.out.println("En esta ocasión en un castillo medieval, recien liberado de tu cautiverio en el calabozo.");
+        System.out.println("Escribe 'help' si necesitas ayuda.");
         System.out.println();
         printLocationInfo();
     }
@@ -125,7 +141,7 @@ public class Game
         boolean wantToQuit = false;
 
         if(command.isUnknown()) {
-            System.out.println("I don't know what you mean...");
+            System.out.println("No se lo que quieres decir...");
             return false;
         }
 
@@ -136,14 +152,23 @@ public class Game
         else if (commandWord.equals("go")) {
             goRoom(command);
         }
-        else if (commandWord.equals("look")) {	
+        else if (commandWord.equals("look")) {  
             look();
         }
-        else if (commandWord.equals("eat")) {	
+        else if (commandWord.equals("eat")) {   
             eat();
         }
-        else if (commandWord.equals("back")) {	
+        else if (commandWord.equals("back")) {  
             back();
+        }
+        else if (commandWord.equals("take")) {
+            take(command);
+        }
+        else if (commandWord.equals("drop")) {
+            drop(command);
+        }
+        else if (commandWord.equals("items")) {  
+            items();
         }
         else if (commandWord.equals("quit")) {
             wantToQuit = quit(command);
@@ -161,10 +186,9 @@ public class Game
      */
     private void printHelp() 
     {
-        System.out.println("You are lost. You are alone. You wander");
-        System.out.println("around at the university.");
+        System.out.println("Estas perdido. Estas solo. Estas deambulando dentro del castillo");
         System.out.println();
-        System.out.println("Your command words are:");
+        System.out.println("Tus comandos son:");
         System.out.println(parser.getCommands());
     }
 
@@ -176,7 +200,7 @@ public class Game
     {
         if(!command.hasSecondWord()) {
             // if there is no second word, we don't know where to go...
-            System.out.println("Go where?");
+            System.out.println("¿A donde quieres ir?");
             return;
         }
 
@@ -186,7 +210,7 @@ public class Game
         Room nextRoom = currentRoom.getExit(direction);
 
         if (nextRoom == null) {
-            System.out.println("There is no door!");
+            System.out.println("¡Ahi no hay salida!");
         }
         else {
             previousRoom.push(currentRoom);
@@ -203,7 +227,7 @@ public class Game
     private boolean quit(Command command) 
     {
         if(command.hasSecondWord()) {
-            System.out.println("Quit what?");
+            System.out.println("¿Que quieres quitar?");
             return false;
         }
         else {
@@ -217,30 +241,182 @@ public class Game
     private void printLocationInfo(){
         System.out.println(currentRoom.getLongDescription());
     }
-    
+
     /** 
      * Print information of the location
      */
-    private void look() {	
+    private void look() {   
         System.out.println(currentRoom.getLongDescription());
     }
-    
+
     /** 
      * Imprime un texto sobre la accion de comer
      */
     private void eat(){
-        System.out.println("You have eaten now and you are not hungry any more");
+        System.out.println("Has comido ahora y ya no tienes hambre.");
     }
-    
+
     /** 
      * Vuelve a la habitacion anterior
      */
     private void back(){
+        //comprueba si la pila previousRoom esta vacia y si no volvemos a la ultima posicion
         if(previousRoom.isEmpty()){
             System.out.println("Estas en el principio.");
         }else{
             currentRoom = previousRoom.pop();
             printLocationInfo();
         }
+    }
+
+    /**
+     * Metodo de acciones para cuando ejecutamos el comando take.
+     * @param command El comando ha ser procesado.
+     */
+    public void take(Command command){
+        //Comprobamos que la habitacion tiene objetos
+        if(currentRoom.getListItemSize() > 0){
+            if(command.hasSecondWord()){
+                String itemName = command.getSecondWord();
+                // buscamos el objeto en la habitacion para cogerlo
+                Item itemSelected = currentRoom.searchItem(itemName);
+                if(itemSelected == null) {
+                    System.out.println("El objeto no esta en la habitacion.");
+                }else{
+                    //comprobamos si el peso no excederia el maximo que podamos llevar
+                    if(canTake(itemSelected)){
+                        itemBackpack.add(itemSelected);
+                        currentRoom.deleteItem(itemSelected);
+                        System.out.println("Objeto " + itemSelected.getItemDescription() + " cogido");
+                    }else if(itemSelected.getItemWeight() > MAX_WEIGHT){
+                        System.out.println("El objeto es muy pesado para llevarlo.");
+                    }else{
+                        System.out.println("El objeto supera nuestra capacidad."); 
+                    }
+                }
+            }else{
+                // si no tiene segunda palabra no sabemos que coger...
+                System.out.println("¿Que quieres coger?");
+                return;
+            }
+        }else{
+            // Si no hay posibles items para coger mostramos un mensaje
+            System.out.println("No hay objetos para coger en la habitación");
+        }
+    }
+
+    /**
+     * Metodo de acciones para cuando ejecutamos el comando drop.
+     * @param command El comando ha ser procesado.
+     */
+    public void drop(Command command){
+        if(command.hasSecondWord()){
+            String itemName = command.getSecondWord();
+            // buscamos el objeto en la mochila para dejarlo
+            Item itemSelected = searchItem(itemName);
+            if (itemSelected == null) {
+                System.out.println("El objeto no esta en la mochila.");
+            }
+            else {
+                currentRoom.addItem(itemSelected);
+                deleteItem(itemSelected);
+                System.out.println("Objeto " + itemSelected.getItemDescription() + " dejado");
+            }
+        }else{
+            // si no tiene segunda palabra no sabemos que coger...
+            System.out.println("¿Que quieres dejar?");
+            return;
+        }
+    }
+
+    /**
+     * Metodo para buscar un item en la mochila.
+     * @param name El nombre del item que buscamos.
+     */
+    public Item searchItem(String name){
+        //buscamos el item en la mochila y los devolvemos si lo encontramos o null en caso de que no este
+        Item itemSelected = null;
+        for(Item itemTemp : itemBackpack){
+            if(itemTemp.getItemDescription().equals(name)){
+                itemSelected = itemTemp;
+            }
+        }
+        return itemSelected;
+    }
+
+    /**
+     * Metodo que elimina un item de la mochila.
+     * @param command El nombre del item que buscamos.
+     */
+    public void deleteItem(Item item){
+        //buscamos el item en la mochila y lo eliminamos si lo encontramos
+        int cont = 0;
+        boolean eliminado = false;
+        while(cont < itemBackpack.size() && !eliminado){
+            if(itemBackpack.get(cont).getItemDescription().equals(item.getItemDescription())){
+                itemBackpack.remove(cont);
+                eliminado = true;
+            }
+            cont++;
+        }
+    }
+
+    /**
+     * Metodo que nos muestra los items de la mochila en el caso de que los haya.
+     */
+    public void items(){
+        //SI hay objetos los mostramos junto con sus caracteristicas y el peso total que haya
+        if(itemBackpack.size() > 0){
+            System.out.println("Tienes en la mochila:");
+            for(Item itemTemp : itemBackpack){
+                System.out.println("Objeto - " + itemTemp.getItemDescription() + getBlankSpaces(itemTemp.getItemDescription() ) + "| Peso - " + itemTemp.getItemWeight());
+            }
+            System.out.println("                 Peso total - " + loadedWeight());
+        }else{
+            System.out.println("No tienes objetos en la mochila.");
+        }
+    }
+
+    /**
+     * Metodo que nos indica si un item excederia el peso y si podria ser cogido.
+     * @param command El item del que queremos evaluar el peso.
+     * @return true Si al coger el item no excedemos el peso que podemos llevar en la mochila.
+     */
+    public boolean canTake(Item item){
+        // Comprobamos si el peso de la mochila sumado al del objeto no excede del maximo transportable y devolvemos true si no lo excede 
+        // o false en caso contrario
+        boolean acceptedWeight = false;
+        if(loadedWeight() + item.getItemWeight() <= MAX_WEIGHT){
+            acceptedWeight = true;
+        }
+        return acceptedWeight;
+    }
+
+    /**
+     * Metodo que nos devuelve el peso que llevamos en la mochila en ese momento.
+     * @return weight El peso total de los items que llevamos en ese momento.
+     */
+    public int loadedWeight(){
+        // Suma los pesos de todos los objetos de la mochila y devuelve la suma. Si no hay objetos devuelve 0.
+        int weight = 0;
+        if(itemBackpack.size() > 0){
+            for(Item itemTemp : itemBackpack){
+                weight += itemTemp.getItemWeight();
+            }
+        }
+        return weight;
+    }
+    
+    /**
+     * Metodo que nos devuelve los espacios en blanco necesarios para que quede colocada la lista de items.
+     * @param name El nombre del item que se va a poner.
+     * @return String Los espacios en blanco necesarios para que quede colocado el item.
+     */
+    public String getBlankSpaces(String name){
+        String blankSpaces = "";
+        for(int cont = 0; cont < DISTANCE - name.length(); cont++){
+            blankSpaces += " ";
+        }
+        return blankSpaces;
     }
 }
--- Room.java
+++ Room.java
@@ -15,10 +15,13 @@ import java.util.*;
  */
 public class Room 
 {
+    // Descripcion de la habitacion
     private String description;
+    // Salidas de la habitacion
     private HashMap<String, Room> roomExits;
+    // Items que hay en la habitacion
     private ArrayList<Item> listItem;
-    
+
     /**
      * Create a room described "description". Initially, it has
      * no exits. "description" is something like "a kitchen" or
@@ -31,11 +34,49 @@ public class Room
         roomExits = new HashMap<String, Room>();
         listItem = new ArrayList<Item>();
     }
-    
+
+    /**
+     * Añade un item a la habitacion.
+     * @param item El item a añadir.
+     */
     public void addItem(Item item){
+        //añadimos el item a la lista de ellos en la habitacion.
         listItem.add(item);
     }
-    
+
+    /**
+     * Elimina un item de la habitacion.
+     * @param item El item a eliminar.
+     */
+    public void deleteItem(Item item){
+        //buscamos el item la lista y si lo encontramos lo eliminamos
+        int cont = 0;
+        boolean eliminado = false;
+        while(cont < listItem.size() && !eliminado){
+            if(listItem.get(cont).getItemDescription().equals(item.getItemDescription())){
+                listItem.remove(cont);
+                eliminado = true;
+            }
+            cont++;
+        }
+    }
+
+    /**
+     * Busca un item en la habitacion.
+     * @param name El nombre del item a buscar.
+     * @return Item Devuelve el item si lo encuentra o null si no lo encuntra
+     */
+    public Item searchItem(String name){
+        //Busca el item en la lista y lo devuelve en caso de encontrarlo o devuelve null si no lo encuentra
+        Item itemSelected = null;
+        for(Item itemTemp : listItem){
+            if(itemTemp.getItemDescription().equals(name)){
+                itemSelected = itemTemp;
+            }
+        }
+        return itemSelected;
+    }
+
     /**
      * Define una salida para la habitacion.
      * @param direction El nombre de la direccion de la salida
@@ -44,7 +85,7 @@ public class Room
     public void setExit(String direction, Room neighbor){
         roomExits.put(direction, neighbor);
     }
-    
+
     /**
      * @return The description of the room.
      */
@@ -68,6 +109,7 @@ public class Room
      * @return Una descripción de las salidas existentes.
      */
     public String getExitString(){
+        // Recorre el hashMaps de salidas de la habitacion y devuelve el conjunto de las que no son null
         String exits = "";
         for(Map.Entry<String, Room> exitTemp : roomExits.entrySet()){
             if(exitTemp.getValue() != null)
@@ -80,14 +122,27 @@ public class Room
      * Devuelve un texto con la descripcion larga de la habitacion del tipo:
      *     You are in the 'name of room'
      *     Exits: north west southwest
-     * @return Una descripcion de la habitacion incluyendo sus salidas
+     * @return Una descripcion de la habitacion incluyendo sus salidas e items
      */
     public String getLongDescription(){
         String longDescription = "\nEstas en " + getDescription() + "\n";
-        for(Item itemTemp : listItem){
-            longDescription += "Contiene " + itemTemp.getItemDescription() + " cuyo peso es de " + itemTemp.getItemWeight() + "\n";
+        if(listItem.size() > 0){
+            longDescription += "     Contiene :\n"; 
+            for(Item itemTemp : listItem){
+                longDescription += "     *  " + itemTemp.getItemDescription() + " cuyo peso es de " + itemTemp.getItemWeight() + "\n";
+            }
+        }else{
+            longDescription += "No hay objetos en la habitacion.\n";
         }
         longDescription += "Salidas:" + getExitString() + "\n";
         return longDescription;
+    }
+
+    /**
+     * Devuelve el numero de items que hay en la habitacion
+     * @return El tamaño del array donde estan guardados los items de la habitacion
+     */
+    public int getListItemSize(){
+        return listItem.size();
     }
 }
